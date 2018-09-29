4d30f0957c4a969856dfeed5743e46193fdb11ba
Rachel Wyatt <rw126@evansville.edu>
2018-09-29 13:43:42 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,13 @@
 class Backer
-attr_accessor :name, :backed_projects, :Project
+attr_accessor :name, :backed_projects, :project
 
   def initialize(name)
     @name = name
     @backed_projects = []
   end
 
-  def back_project(Project)
-    @backed_projects << Project
+  def back_project(project)
+    @backed_projects << project
   end
 
 
